23d1362464d89d9e1a58e691eb042342a38b95d5
Federico Berasategui <fberasategui@lexim.com.ar>
2018-12-04 12:54:54 -0300

    Exception handling sample

---
+++ MercadoPagoSDK.Samples/Samples/ExceptionSample.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MercadoPago;
+using MercadoPago.Resources;
+
+namespace MercadoPagoSDK.Samples
+{
+    internal class ExceptionSample : ISample
+    {
+        public string Category => "Error Handling";
+        public string Name => "Handling Exceptions";
+
+        public void Run()
+        {
+            try
+            {
+                var invalid_id = 1234;
+                var payment = Payment.FindById(invalid_id);
+            }
+            catch (MPException e) when (e.Error != null)
+            {
+                // Bad Request exceptions will have an Error property
+                Console.WriteLine(e.Error.ToString());
+            }
+            catch (MPException e)
+            {
+                // For other exception types
+                Console.WriteLine($"{e.ErrorMessage} - {e.Message}");
+            }
+        }
+    }
+}
