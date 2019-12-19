7261480e3bf68e621e5d8ee4d9457f3561eeca77
Lautaro Tosi <ltosi95@hotmail.com>
2019-12-19 16:18:27 -0300

    Fixed PaymentMethods thumbnails Added Sites Added PaymentMethods sample

---
+++ MercadoPagoSDK.Samples/Samples/PaymentMethods.cs
@@ -0,0 +1,19 @@
+﻿using MercadoPago.DataStructures.Payment;
+using MercadoPago.Resources;
+using System.Collections.Generic;
+
+namespace MercadoPagoSDK.Samples
+{
+    internal class PaymentMethods : ISample, IRequiresAccessToken
+    {
+        public string Name => "Get Payment Methods";
+
+        public string Category => "Payment Methods";
+
+        public void Run()
+        {
+            List<PaymentMethod> list = PaymentMethod.All(MercadoPago.Common.Sites.MLA);   
+
+        }
+    }
+}
